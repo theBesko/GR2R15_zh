b7d2ea7b85be669a584f87d5f694aefebccb3513
theBesko <74558825+theBesko@users.noreply.github.com>
2021-05-05 16:58:39 +0200

    Add files via upload

--- database.js
+++ database.js
@@ -3,13 +3,14 @@ var strArr = [];
 var finalArr = [];
 
 function readCSV(filename) {
+    finalArr=null;
+    finalArr=[];
   xhttp.onreadystatechange = function () {
     if (xhttp.readyState == 4 && xhttp.status == 200) {
       strArr = this.responseText.split("\n");
       for (let i = 0; i < strArr.length; i++) {
         finalArr.push(strArr[i].split(";"));
       }
-      
     }
   };
 
@@ -19,9 +20,16 @@ function readCSV(filename) {
 
 readCSV("./sg500.csv");
 console.log(finalArr);
-readCSV("./sg500/pcaResult.csv");
-console.log(finalArr);
-readCSV("./sg500/svmResult.csv");
-console.log(finalArr);
-readCSV("./sg500/out.csv");
-console.log(finalArr);
+
+setTimeout(() => {
+  readCSV("./sg500/pcaResult.csv");
+  console.log(finalArr);
+}, 1000);
+setTimeout(() => {
+  readCSV("./sg500/svmResult.csv");
+  console.log(finalArr);
+}, 2000);
+setTimeout(() => {
+  readCSV("./sg500/out.csv");
+  console.log(finalArr);
+}, 3000);
